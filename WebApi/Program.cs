309ee711193ac2ad8c90f1ba738c41b31ed98c46
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-07-25 14:38:05 +0600

    й

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using Persistence;
 
@@ -22,6 +23,21 @@ builder.Services.AddSwaggerGen(c =>
         Title = "OnionArchitecture",
     });
 
+});
+#endregion
+
+#region API Versioning
+// Add API Versioning to the Project
+builder.Services.AddApiVersioning(config =>
+{
+    // Specify the default API Version as 1.0
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+
+    // If the client hasn't specified the API version in the request, use the default API version number 
+    config.AssumeDefaultVersionWhenUnspecified = true;
+
+    // Advertise the API versions supported for the particular endpoint
+    config.ReportApiVersions = true;
 });
 #endregion
 
