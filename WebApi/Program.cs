e89df300098782d05eeaf191dafd8517169b5bbe
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-07-25 14:12:01 +0600

    2

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,14 +1,17 @@
+using Application;
 using Microsoft.OpenApi.Models;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+//builder.Services.AddSwaggerGen();
 #region Swagger
 builder.Services.AddSwaggerGen(c =>
 {
